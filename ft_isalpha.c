05c56126f3ea130e4ae93c531a9d2a0a20f40a87
tuttigiuperterraa <105778039+tuttigiuperterraa@users.noreply.github.com>
2023-10-21 10:03:08 +0200

    Update ft_isalpha.c - done

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -10,18 +10,12 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 int	ft_isalpha(int a)
 {
 	if(!((a >= 'A' && a <= 'Z') || (a >= 'a' && a <= 'z')))
-		return (0);
+		return (1);
 	else
-		return (a);
+		return (0);
 }
-
-/*int	main(void)
-{
-	int	p;
-
-	p = ft_isalpha ('q');
-	printf("%d", p);
-}*/
