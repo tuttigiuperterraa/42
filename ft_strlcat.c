e06f05f2c27e0142a0c0cce9d45ff40c0f41e090
tuttigiuperterraa <105778039+tuttigiuperterraa@users.noreply.github.com>
2023-10-21 11:07:04 +0200

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -9,8 +9,8 @@
 /*   Updated: 2023/10/17 18:18:23 by gcosenza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-//#include <stdio.h>
-//#include <bsd/string.h>
+
+#include "libft.h"
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
@@ -18,16 +18,12 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	size_t	len_src;
 	size_t	i;
 
-	len_dst = 0;
-	while (dst[len_dst] != '\0')
-		len_dst++;
-	len_src = 0;
-	while (src[len_src] != '\0')
-		len_src++;
+	len_dst = ft_strlen(dst);
+	len_src = ft_strlen(src);
 	if (size < len_src + 1)
 		return (len_dst);
 	i = 0;
-	while (src[i] != '\0' && len_dst + i <= size)
+	while (src[i] != '\0' && len_dst + i < size - 1)
 	{
 		dst[len_dst + i] = src[i];
 		i++;
