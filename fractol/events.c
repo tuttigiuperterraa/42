b3cbfe514781c5e846be228645a6f512e30bc3ec
tuttigiuperterraa <105778039+tuttigiuperterraa@users.noreply.github.com>
2024-01-13 16:15:36 +0100

    Update events.c

--- fractol/events.c
+++ fractol/events.c
@@ -37,10 +37,20 @@ int	key_handler(int keysym, t_fractal *fractal)
 
 int	mouse_handler(int button, int x, int y, t_fractal *fractal)
 {
+	//double mouse_real = scaling(fractal->mouse_x, -2, 2, 0, WIDTH);
+    	//double mouse_imag = scaling(fractal->mouse_y, 2, -2, 0, HEIGHT);
 	if (button == Button5)
-		fractal->zoom *= 0.95;
+	{
+		fractal->zoom *= 0.95 
+		//fractal->shift_x += (mouse_real - WIDTH / 2) * fractal->zoom;
+		//fractal->shift_y += (mouse_imag - HEIGHT / 2) * fractal->zoom;
+	}
 	else if (button == Button4)
-		fractal->zoom *= 1.05;
+	{
+		fractal->zoom *= 1.05 
+		//fractal->shift_x -= (mouse_real - WIDTH / 2) * fractal->zoom;
+		//fractal->shift_y -= (mouse_imag - HEIGHT / 2) * fractal->zoom;
+	}
 	fractal_render(fractal);
 	return (0);
 }
@@ -49,9 +59,12 @@ int	julia_track(int x, int y, t_fractal *fractal)
 {
 	if (!ft_strncmp(fractal->name, "julia", 5))
 	{
-		fractal->julia_x = (map(x, -2, +2, 0, WIDTH) * fractal->zoom) + fractal->shift_x;
-		fractal->julia_y = (map(y, +2, -2, 0, HEIGHT) * fractal->zoom) + fractal->shift_y;
+		fractal->julia_x = (scaling(x, -2, +2, 0, WIDTH) * fractal->zoom) + fractal->shift_x;
+		fractal->julia_y = (scaling(y, +2, -2, 0, HEIGHT) * fractal->zoom) + fractal->shift_y;
 		fractal_render(fractal);
 	}
+	//salvo le coordinate mouse in una variabile e al momento dello zoom le uso per ricentrare l'immagine
+	// fractal-> mouse_x =(scaling(x, -2, +2, 0, WIDTH) * fractal->zoom) + fractal->shift_x; //TODO
+	// fractal-> mouse_y = (scaling(y, +2, -2, 0, HEIGHT) * fractal->zoom) + fractal->shift_y; //TODO
 	return 0;
 }
