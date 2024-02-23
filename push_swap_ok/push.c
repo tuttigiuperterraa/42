0f7e95b235a52b4736c054b6a48104e793e5c10f
tuttigiuperterraa <105778039+tuttigiuperterraa@users.noreply.github.com>
2024-02-23 08:40:21 +0100

    Update push.c

--- push_swap_ok/push.c
+++ push_swap_ok/push.c
@@ -72,3 +72,66 @@ void pb(t_stack **add, t_stack **take)
 	remove_first(take);
 	write(1, "pb\n", 3);
 }
+
+int find_direction_push(t_stack *a, int push, int A)
+{
+	int pi;
+	int pj;
+
+	pi = penality_r_push(a, push);
+	pj = penality_rr_push(a, push);
+    if (pj < pi)
+	{
+		if (A)
+			return (4);
+		return (5);
+	}  
+    else 
+	{
+		if (A)
+			return (1);
+	}
+	return (2);
+}
+
+int penality_r_push(t_stack *a, int t)
+{
+    int pi;
+    int i;
+    t_stack *head;
+    
+    head = a;
+    i = 0;
+    pi = 0;
+    while (i++ < counter(a)/2)
+    {
+        if (head->push != t)
+        {
+            pi++;
+            head = head->next;
+        } 
+    }
+	return (pi); 
+}
+
+int penality_rr_push(t_stack *a, int t)
+{
+    int pj;
+    int j;
+    t_stack *mid;
+    
+    mid = a;
+    j = 0;
+    pj = 0;
+    while (j++ < counter(a)/2 -1)
+        mid = mid->next;
+    while (mid)
+    {
+        if (mid->push != t)
+        {
+            pj++;
+            mid = mid->next;
+        }
+        return (counter(a)/2 -pj);
+    }
+}
