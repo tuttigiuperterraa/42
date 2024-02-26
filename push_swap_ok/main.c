1438bb96f0c32a1c1eabbf8955c48c928d716831
tuttigiuperterraa <105778039+tuttigiuperterraa@users.noreply.github.com>
2024-02-26 08:09:26 +0100

    Add files via upload

---
+++ push_swap_ok/main.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gcosenza <gcosenza@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/23 15:11:23 by aconti            #+#    #+#             */
+/*   Updated: 2024/02/26 07:30:34 by gcosenza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	is_sorted(long int *arr, int n)
+{
+	int	i;
+
+	i = 1;
+	while (i < n)
+	{
+		if (arr[i] < arr[i - 1])
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	main(int argc, char **argv)
+{
+	t_stack		*stack_a = NULL;
+	t_stack		*stack_b = NULL;
+	int			n;
+
+	n = count_num(argv);
+	long int	nums[n];
+	ft_atoi(argv, nums);
+	if (ft_check(nums, n) == 0 || argc < 2)
+		return (write(1, "Error", 5));
+	if (n == 1 || is_sorted(nums, n))
+		return (0);
+	array_to_stack(nums, n, &stack_a);
+	sort(&stack_a, &stack_b, n);
+	return (0);
+}
